7fb3f12c0ebd559b31fa5dffb03df70ef0deed21
maheshg23 <gmn1994@gmail.com>
2020-12-06 18:56:12 -0500

    Update Project.py

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import psycopg2
 import streamlit as st
 from configparser import ConfigParser
 
-'# Demo: Streamlit + Postgres'
+'# RESTAURANT SEARCH APPLICATION'
 
 @st.cache
 def get_config(filename='database.ini', section='postgresql'):
@@ -43,20 +43,19 @@ def query_db(sql: str):
 
     return df
 
+'## Display Tables'
 
-'## Read tables'
-
-sql_all_table_names = "select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';"
+sql_all_table_names = "SELECT relname FROM pg_class WHERE relkind='r' AND relname !~ '^(pg_|sql_)';"
 all_table_names = query_db(sql_all_table_names)['relname'].tolist()
 table_name = st.selectbox('Choose a table', all_table_names)
 if table_name:
-    f'Display the table'
-
-    sql_table = f'select * from {table_name};'
+    f'Display All Table Infromation'
+    sql_table = f'SELECT * FROM {table_name};'
     df = query_db(sql_table)
     st.dataframe(df)
 
-'## Query 1 - List the top 5 restaurants based on the average user reviews'
+'## Query 1'
+'### List the top 5 restaurants based on the average user reviews'
 
 db_query1 = f"""SELECT avg_rating,* FROM restaurants JOIN ( 
                 SELECT res_id, avg(rating) AS avg_rating FROM reviews 
@@ -70,12 +69,12 @@ df = query_db(db_query1)
 st.dataframe(df)
 
 '## Query 2 - Photos and Reviews'
-'## List all reviews and photos posted for each restaurant along with the username who posted it' 
+'### List all reviews and photos posted for each restaurant along with the username who posted it' 
 
 tables_to_sel = ['reviews', 'photos']
-table_sel = st.radio('choose Review or Photos', tables_to_sel)
+table_sel = st.radio('Choose Review or Photos', tables_to_sel)
 
-sql_restaurant_names = 'select name from restaurants;'
+sql_restaurant_names = 'SELECT name FROM restaurants;'
 restaurant_names = query_db(sql_restaurant_names)['name'].tolist()
 restaurant_names.insert(0, "All")
 restaurant_name = st.selectbox('Choose a Restaurant', restaurant_names)
@@ -93,44 +92,125 @@ if table_sel == 'reviews':
                         ORDER BY res.id;"""
 
 else:
-    db_sql_query = f""" SELECT res.id, res.name, p.pid, p.url, u.uid, u.username 
-                        FROM restaurants AS res, photos AS p, users AS u 
-                        WHERE res.id = p.res_id AND u.uid = p.user_id and res.name = '{restaurant_name}'
-                        ORDER BY res.id;"""
+    if (restaurant_name == 'All'):
+        db_sql_query = f""" SELECT res.id, res.name, p.pid, p.url, u.uid, u.username 
+                            FROM restaurants AS res, photos AS p, users AS u 
+                            WHERE res.id = p.res_id AND u.uid = p.user_id 
+                            ORDER BY res.id;"""
+    else:                    
+        db_sql_query = f""" SELECT res.id, res.name, p.pid, p.url, u.uid, u.username 
+                            FROM restaurants AS res, photos AS p, users AS u 
+                            WHERE res.id = p.res_id AND u.uid = p.user_id and res.name = '{restaurant_name}'
+                            ORDER BY res.id;"""
 
 df = query_db(db_sql_query)
 st.dataframe(df)
 
 
+'## Query 3'
+'### Search with Restaurant Name, City, Country, Status, Tags, Payment Method'
+
+search_key = ['Restaurant Name','City','Country','Status','Tags','Payment Method']
+search_sel = st.radio('Choose a Search', search_key)
+
+if search_sel == 'Restaurant Name':
+    sql_restaurant_names = 'SELECT name FROM restaurants;'
+    search_list = query_db(sql_restaurant_names)['name'].tolist()
+
+if search_sel == 'City':
+    sql_cities = 'SELECT DISTINCT city FROM location;'
+    search_list = query_db(sql_cities)['city'].tolist()
+
+if search_sel == 'Country':
+    sql_countries = 'SELECT DISTINCT country FROM location;'
+    search_list = query_db(sql_countries)['country'].tolist()
+
+if search_sel == 'Status':
+    sql_statuses = 'SELECT DISTINCT name FROM status;'
+    search_list = query_db(sql_statuses)['name'].tolist()``
+
+if search_sel == 'Tags':
+    sql_tag_names = 'SELECT DISTINCT name FROM tags;'
+    search_list = query_db(sql_tag_names)['name'].tolist()
+
+if search_sel == 'Payment Method':
+    sql_payment_methods = 'SELECT DISTINCT name FROM payment_methods;'
+    search_list = query_db(sql_payment_methods)['name'].tolist()
+
+search_list_sel = st.selectbox(f'Choose a {search_sel}', search_list)
+
+# country_sel = st.radio('Choose a Country', countries)
+# city_sel = st.radio('Choose a City', cities)
+# tag_name_sel = st.selectbox('Choose an Restaurant Tag', tag_names)
+# status_sel = st.selectbox('Choose an Restaurant Status', statuses)
+# payment_method_sel = st.selectbox('Choose an Restaurant Status', payment_methods)
+
+db_query = f""" SELECT * FROM   restaurants AS res, status AS s, location AS l, 
+                                restaurant_payments_mapping AS rpm, payment_methods AS pm,
+                                restaurant_tags_mapping as rtm, tags AS t
+                where   res.status = s.sid AND 
+                        res.location = l.lid AND 
+                        res.id = rpm.res_id AND 
+                        rpm.payment_id = pm.pmid AND 
+                        res.id = rtm.res_id AND
+                        rtm.tag_id = t.tid AND """;
+    
+
+if search_sel == 'Restaurant Name':
+    select_query = db_query + f"res.name = '{search_list_sel}';"
+
+if search_sel == 'City':
+    select_query = db_query + f"l.city = '{search_list_sel}';"
+
+if search_sel == 'Country':
+    select_query = db_query + f"l.country = '{search_list_sel}';"
+
+if search_sel == 'Status':
+    select_query = db_query + f"s.name = '{search_list_sel}';"
+
+if search_sel == 'Tags':
+    select_query = db_query + f"t.name = '{search_list_sel}';"
+
+if search_sel == 'Payment Method':
+    select_query = db_query + f"pm.name = '{search_list_sel}';"
+
+'#### Result'
+df = query_db(select_query)
+st.dataframe(df)
+# st.dataframe(df,2000,1000)
+
+# if country_sel: 
+#     db_query = f""" SELECT * FROM restaurants AS res, status AS s, location AS l, restaurant_payments_mapping AS rpm, payment_methods AS pm
+#                     where   res.status = s.sid AND 
+#                             res.location = l.lid AND 
+#                             res.id = rpm.res_id AND 
+#                             rpm.payment_id = pm.pmid AND 
+#                             l.country = '{country_sel}';"""                  
+#     df = query_db(db_query)
+#     st.dataframe(df)
+
+# if city_sel: 
+#     db_query = f""" SELECT * FROM restaurants AS res, status AS s, location AS l, restaurant_payments_mapping AS rpm, payment_methods AS pm
+#                     where   res.status = s.sid AND 
+#                             res.location = l.lid AND 
+#                             res.id = rpm.res_id AND 
+#                             rpm.payment_id = pm.pmid AND 
+#                             l.city = '{city_sel}';"""              
+#     df = query_db(db_query)
+#     st.dataframe(df)
 
 
-'## Demo Select Orders' 
 
-sql_order_ids = 'select order_id from orders;'
-order_ids = query_db(sql_order_ids)['order_id'].tolist()
-order_id = st.selectbox('Choose an order', order_ids)
-if order_id:
-    sql_order = f"""select C.name, O.order_date
-                    from orders as O, customers as C 
-                    where O.order_id = {order_id}
-                    and O.customer_id = C.id;"""
+# '## Query Restaurants'
 
-    customer_info = query_db(sql_order).loc[0]
-    customer_name = customer_info['name']
-    order_date = customer_info['order_date']
-    st.write(f'This order is placed by {customer_name} on {order_date}.')
-
-
-'## Query Restaurants'
-
-sql_restaurant_names = 'select name from restaurants;'
-restaurant_names = query_db(sql_restaurant_names)['name'].tolist()
-restaurant_name = st.selectbox('Choose a Restaurant', restaurant_names)
-if restaurant_name:
-    sql_restaurant = f"select * from restaurants where name = '{restaurant_name}';"
-    customer_info = query_db(sql_restaurant).loc[0]
-    c_address, c_city, c_state = customer_info['address'], customer_info['phone'], customer_info['owner_id']
-    st.write(f"{customer_name} is at {c_address}, and contact in {customer_info['phone']}, {customer_info['owner_id']}.")
+# sql_restaurant_names = 'select name from restaurants;'
+# restaurant_names = query_db(sql_restaurant_names)['name'].tolist()
+# restaurant_name = st.selectbox('Choose a Restaurant', restaurant_names)
+# if restaurant_name:
+#     sql_restaurant = f"select * from restaurants where name = '{restaurant_name}';"
+#     customer_info = query_db(sql_restaurant).loc[0]
+#     c_address, c_city, c_state = customer_info['address'], customer_info['phone'], customer_info['owner_id']
+#     st.write(f"{customer_name} is at {c_address}, and contact in {customer_info['phone']}, {customer_info['owner_id']}.")
 
 
 '## Demo Query customers'
--- queries.sql
+++ queries.sql
@@ -18,4 +18,33 @@ SELECT res.id, res.name, p.pid, p.url, u.uid, u.username FROM restaurants AS res
 WHERE res.id = p.res_id AND u.uid = p.user_id
 ORDER BY res.id;
 
+-- Search with Restaurant Name, City, Country, Status, Tags, Payment Method
+
+SELECT * FROM   restaurants AS res, status AS s, location AS l, 
+                restaurant_payments_mapping AS rpm, payment_methods AS pm,
+                restaurant_tags_mapping as rtm, tags AS t
+where   res.status = s.sid AND 
+        res.location = l.lid AND 
+        res.id = rpm.res_id AND 
+        rpm.payment_id = pm.pmid AND 
+        res.id = rtm.res_id AND
+        rtm.tag_id = t.tid AND
+        (res.name = 'Indian' OR 
+        l.city = 'Seattle' OR
+        l.country = 'USA' OR
+        s.name = 'Open' OR
+        t.name = 'Indian' OR
+        pm.name = 'Cash' OR
+        );
+
+SELECT * FROM   restaurants AS res, status AS s, location AS l, 
+                restaurant_payments_mapping AS rpm, payment_methods AS pm,
+                restaurant_tags_mapping as rtm, tags AS t
+where   res.status = s.sid AND 
+        res.location = l.lid AND 
+        res.id = rpm.res_id AND 
+        rpm.payment_id = pm.pmid AND 
+        res.id = rtm.res_id AND
+        rtm.tag_id = t.tid AND
+        l.city = 'Seattle';
 
